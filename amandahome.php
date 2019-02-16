f00fbc4dcf720f7f14c3689865e820fe74a587fb
Jacob Lee <wlee33@u.rochester.edu>
2019-02-16 18:51:49 -0500

    changed just the home page to use 3 column CSS grid

--- amandahome.php
+++ amandahome.php
@@ -3,4 +3,4 @@
         <h2>Introduction</h2>
         <p>I am a student here at University of Rochester finishing up my major in Brain & Cognitive Sciences as well as minors in Computer Science and Psychology.  I knew coming into college that I wanted to major in Brain & Cognitive Sciences because it seemed like a combination of biology and psychology—two subjects I had loved in high school.  I had originally planned on double majoring in psychology along with my Brain & Cognitive Sciences major, but when I took The Science of Programming (CSC 171) as part of my Brain & Cognitive Sciences program I loved it and decided to stick with it.  I’m very glad I took that class early on in my college career so that I was able to add a Computer Science minor and explore that newfound interest.</p>
     </main>
-    <?php include "inc/footamanda.inc"; ?>
+<?php include "inc/footamanda.inc"; ?>
--- index.php
+++ index.php
@@ -7,75 +7,72 @@
   <link rel="stylesheet" href="css/styles.css">
 </head>
 
-<nav class="menu">
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="amandahome.php">Amanda</a></li>
-    <li><a href="mikehome.php">Michael</a></li>
-    <li><a href="tomhome.php">Tom</a></li>
-  </ul>
-</nav>
+<body class="container">
+  <nav class="menu">
+    <ul>
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="amandahome.php">Amanda</a>
+      </li>
+      <li>
+        <a href="mikehome.php">Michael</a>
+      </li>
+      <li>
+        <a href="tomhome.php">Tom</a>
+      </li>
+    </ul>
+  </nav>
+  <h2 class="welcome"><a href="index.php">Welcome to Timbuktu</a></h2>
 
-<body class = "container">
-
-<header>
-  <a href="index.php">
-    
-  </a>
-</header>
-	<h2>Welcome to Timbuktu</h2>
-
-  <div>
-        <a href="amandahome.php">
-            <h1>Amanda</h1>
-            <img src="images/amanda.jpg" alt="Photo of Amanda">
-        </a>
-    </div>
+  <div class="amanda-intro">
+    <a href="amandahome.php">
+      <h1>Amanda</h1>
+      <img src="images/amanda.jpg" alt="Photo of Amanda">
+    </a>
     <aside>
       <ul>
-          <li>Brain & Cognitive Sciences Major</li>
-          <li>Computer Science and Psychology Minors</li>
-          <li>Dancer</li>
+        <li>Brain & Cognitive Sciences Major</li>
+        <li>Computer Science and Psychology Minors</li>
+        <li>Dancer</li>
       </ul>
     </aside>
-
-	<div>
-  <a href="mikehome.php">
-    <h1>Michael</h1>
-    <img src="images/kayak2.jpg" alt="picture of Michael">
-  </a>
   </div>
+  <div class="mike-intro">
+    <a href="mikehome.php">
+      <h1>Michael</h1>
+      <img src="images/kayak2.jpg" alt="picture of Michael">
+    </a>
 
-  <aside>
-    <ul>
-    <li> Environmental Science Major </li>
-    <li> Computer Science Minor </li>
-    <li> Kayaker </li>
-    </ul>
-  </aside>
+    <aside>
+      <ul>
+        <li> Environmental Science Major </li>
+        <li> Computer Science Minor </li>
+        <li> Kayaker </li>
+      </ul>
+    </aside>
+  </div>
+  <div class="tom-intro">
+    <a href="tomhome.php">
+      <h1>Tom</h1>
+      <img src="images/cockpit.jpg" alt="Tom in flight">
+    </a>
 
-  <div>
-  <a href="tomhome.php">
-    <h1>Tom</h1>
-    <img src="images/cockpit.jpg" alt="Tom in flight">
-  </a>
-</div>
+    <aside>
+      <ul>
+        <li>Political Science Major</li>
+        <li>Computer Science Minor</li>
+        <li>Glider Pilot</li>
+      </ul>
+    </aside>
+  </div>
+  <footer class="leftbox">
+    CSC 174: Advanced Front-end Web Design and Developement
+  </footer>
 
-<aside>
-  <ul>
-    <li>Political Science Major</li>
-    <li>Computer Science Minor</li>
-    <li>Glider Pilot</li>
-  </ul>
-</aside>
-
-	<footer class="leftbox">
-  		CSC 174: Advanced Font-end Web Design and Developement 
-	</footer>
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script src="js/menu-highlighter.js"></script>
-
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <script src="js/menu-highlighter.js"></script>
 </body>
 
 </html>
